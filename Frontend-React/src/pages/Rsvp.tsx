cd8a1be0b296510f48c8f247616f64c411a25afd
Abraham John <101580587+Jabrahamjohn@users.noreply.github.com>
2024-04-20 17:38:30 +0300

    this is the rsvp form page (#12)
    Co-authored-by: Mahmud Enesi Abdulazeez <abdulazeezm578@gmail.com>

--- Frontend-React/src/pages/Rsvp.tsx
+++ Frontend-React/src/pages/Rsvp.tsx
@@ -58,6 +58,7 @@ const Rsvp = () => {
                             <input type="radio" name="guest" id="no" value="no" className="mr-3"/>
                             <label htmlFor="no">No</label>
                         </div>
+                        <textarea placeholder="send a congratulatory message" rows={4} cols={50} className="border border-black rounded"></textarea>                   
                         <label htmlFor="quantity" className="font">number of guests</label>
                         <input type="number" id="quantity" name="quantity" min={0} max={100} step={1} className="border border-black rounded" />
                         <textarea placeholder="Guest Names" rows={4} cols={50} className="border border-black rounded"></textarea>                   
