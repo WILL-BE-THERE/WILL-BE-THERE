f9360fa974efbf4a268354b8cdcba774036f014a
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 22:35:25 +0300

    styled the buttons to much specs

--- Frontend-React/src/components/Buttons/RsvpButton.tsx
+++ Frontend-React/src/components/Buttons/RsvpButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const RsvpButton = () => {
   return (
-    <Link to="/Rsvp" className="text-[0.95rem] text-neutral-200">
+    <Link to="/Rsvp" className="px-5 py-2 bg-primary-100 text-white text-[0.95rem] rounded-md">
       Rsvp
     </Link>
   )
