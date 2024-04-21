53ecbd1e0a6ccee6b213a0ea9b8f44a580c52884
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 13:47:30 +0300

    this backbone is unstyled

--- Frontend-React/src/pages/Events.tsx
+++ Frontend-React/src/pages/Events.tsx
@@ -1,4 +1,35 @@
 const Events = () => {
-  return <div>Events</div>
+  return(
+  <section>
+    <div>
+      <h1>Events</h1>
+      <p>Here is a list of all the events that are happening in the near future.</p>
+      <div>
+        <h2>Upcoming Events</h2>
+        <div>
+          <h3>Event 1</h3>
+          <p>Event Description</p>
+          <button>RSVP</button>
+        </div>
+        <div>
+          <h3>Event 2</h3>
+          <p>Event Description</p>
+          <button>RSVP</button>
+        </div>
+        <div>
+          <h3>Event 3</h3>
+          <p>Event Description</p>
+          <button>RSVP</button>
+        </div>
+      </div>
+    </div>
+    <div>
+      <h1> Ready to Take your Events to the Next Level?</h1>
+      <p>Join thousands of organizers and attendees who trust <em>WILL BE THERE</em> for seamless Event management and unforgettable experiences. Whether you're planning an event or looking for your next great gathering, we've got you covered</p>
+      <button>SignUp</button>
+      <button>LogIn</button>
+    </div>
+  </section>
+  )
 }
 export default Events
