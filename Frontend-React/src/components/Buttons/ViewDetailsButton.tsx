889e21555096da712254dc0a6d124dabe0093bc9
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 22:33:54 +0300

    styled the buttons to much specs

--- Frontend-React/src/components/Buttons/ViewDetailsButton.tsx
+++ Frontend-React/src/components/Buttons/ViewDetailsButton.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const ViewDetailsButton = () => {
   return (
-    <Link to="/EventDetails" className="text-[0.95rem] text-neutral-200">
+    <Link to="/EventDetails" className="text-[0.95rem]  px-5 py-2 bg-primary-100 text-white rounded-md">
       View Event Details
     </Link>
   )
