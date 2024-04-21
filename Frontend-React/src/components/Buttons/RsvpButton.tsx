31940c1605999bc306034318083fe4db78e18bec
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 22:19:17 +0300

    added the rsvp button

---
+++ Frontend-React/src/components/Buttons/RsvpButton.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom'
+
+const RsvpButton = () => {
+  return (
+    <Link to="/Rsvp" className="text-[0.95rem] text-neutral-200">
+      Rsvp
+    </Link>
+  )
+}
+export default RsvpButton
