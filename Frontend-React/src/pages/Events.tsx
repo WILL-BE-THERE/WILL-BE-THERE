569c53ab94a4d5a853d3746c5ab29fd5997f6ce5
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 22:11:11 +0300

    event details and events page

--- Frontend-React/src/pages/Events.tsx
+++ Frontend-React/src/pages/Events.tsx
@@ -1,7 +1,10 @@
+import LoginButton from '../components/Buttons/LoginButton'
+import SignUpButton from '../components/Buttons/SignUpButton'
+
 const Events = () => {
   return (
-    <section className="bg-white">
-      <div className="ml-2">
+    <div className="bg-white">
+      <section className="ml-2">
         <h1 className="font-bold">Upcoming Events</h1>
         <p>Here is a list of all the events that are happening in the near future.</p>
         <div className=" border bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
@@ -51,20 +54,29 @@ const Events = () => {
             </button>
           </div>
         </div>
-      </div>
-      <div className="border shadow- shadow-2xl p-10 place-items-center">
-        <div className="items-center p-5 mt-3 ">
-          <p className="font-bold ">Ready to Take your Events to the Next Level?</p>
-          <p className="font-light">Join thousands of organizers and attendees who trust <em className="font-bold">WILL BE THERE</em> for seamless Event management and unforgettable experiences. Whether you're planning an event or looking for your next great gathering, we've got you covered</p>
-          <button className="px-5 py-2 rounded-md bg-white text-black text-sm hover:scale-110 active:scale-105 w-fit transition-all border-none outline-none m-6">
-            SignUp
+      </section>
+      <section className="h-[30rem] bg-white flex items-center justify-center sm:h-[25rem]">
+        <div className="w-[90%] mx-auto flex flex-col text-center lg:w-[60%]">
+          <h1 className="text-2xl font-semibold">
+            Ready to take your <br />
+            events to the next level?
+          </h1>
+          <p className="text-sm text-neutral-200 font-medium w-full mx-auto mt-5 sm:w-4/5">
+            Join thousands of event organizers and attendees who trust us for
+            seamless event management an unforgettable experiences. Whether you
+            are planning an event or looking for your next great gathering,
+            we've got you covered.
+          </p>
+          <div className="mt-10">
+            <button>
+              <SignUpButton />
+              <LoginButton />
             </button>
-          <button className="px-5 py-2 rounded-md bg-primary-100 text-white text-sm hover:scale-110 active:scale-105 w-fit transition-all border-none outline-none m-6">
-            LogIn
-            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+    </div>
   )
 }
 
