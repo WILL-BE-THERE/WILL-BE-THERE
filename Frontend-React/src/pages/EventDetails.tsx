736ea425216f3e04af77785ec8e64cc8466c34ad
Jabrahamjohn <jabrahamjohns@gmail.com>
2024-04-21 22:53:09 +0300

    styled the buttons to much specs

--- Frontend-React/src/pages/EventDetails.tsx
+++ Frontend-React/src/pages/EventDetails.tsx
@@ -10,22 +10,22 @@ const EventDetails = () => {
             </div>
         </aside>
         <aside>
-        <div>
-        <h1>Event Details</h1>
-            <p>Event Name: Event 1</p>
-            <p>Event Description: Event Description</p>
-            <p>Event Date: Date</p>
-            <p>Venue</p>
-            <p>Location</p>
-            <p>Time</p>
-            <p>Dress code</p>
-            <button >
-                <BackButton />
-            </button>
-            <button>
-                <RsvpButton />
-            </button>
-        </div>
+            <div>
+                <h1>Event Details</h1>
+                <p>Event Name: Event 1</p>
+                <p>Event Description: Event Description</p>
+                <p>Event Date: Date</p>
+                <p>Venue</p>
+                <p>Location</p>
+                <p>Time</p>
+                <p>Dress code</p>
+                <button >
+                    <BackButton />
+                </button>
+                <button>
+                    <RsvpButton />
+                </button>
+            </div>
         </aside>
     </section>
   )
