5fb095e3c99446084c3c781a024a1128ede9c827
Mahmud1087 <abdulazeezm578@gmail.com>
2024-04-27 16:17:59 +0100

    added a package for pie chart and finished the event (ticket setting) page

--- Frontend-React/src/pages/dashboard/GuestMetrics.tsx
+++ Frontend-React/src/pages/dashboard/GuestMetrics.tsx
@@ -1,4 +1,17 @@
+import { PieChart } from 'react-minimal-pie-chart'
+
 const GuestMetrics = () => {
-  return <div>GuestMetrics</div>
+  return (
+    <div className="w-28 bg-white h-28 rounded-full relative flex items-center justify-center">
+      <PieChart
+        className="absolute w-56 h-56 -z-[1]"
+        paddingAngle={2}
+        data={[
+          { title: 'One', value: 10, color: '#E38627' },
+          { title: 'Two', value: 15, color: '#C13C37' },
+        ]}
+      />
+    </div>
+  )
 }
 export default GuestMetrics
