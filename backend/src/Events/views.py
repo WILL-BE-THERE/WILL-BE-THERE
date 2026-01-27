e34f8153f411d99129ceb7e7ba9c7b53f91d6b2b
Jabrahamjohn <Jabrahamjohns@gmail.com>
2026-01-27 17:28:35 +0300

    fix: Add JWTAuthentication import to views for authentication handling

--- backend/src/Events/views.py
+++ backend/src/Events/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import (
 )
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Event
 from .serializer import EventSerializer
